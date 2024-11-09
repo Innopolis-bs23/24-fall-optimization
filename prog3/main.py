c2acd333150209bfa024911b693a73049a5ba3b5
Exselomance <150796351+Exselomance@users.noreply.github.com>
2024-11-09 17:37:31 +0300

    Update main.py

--- prog3/main.py
+++ prog3/main.py
@@ -3,31 +3,34 @@ from tabulate import tabulate
 from north_west import north_west_corner_method
 from vogels import vogels_approximation_method
 from russell import russells_approximation_method
+import copy
 
 DEBUG = False
+
 
 def read_data():
     if DEBUG:
         supply = [300, 400, 500]
         demand = [250, 350, 400, 200]
         costs = np.array([
-            [3, 1, 7, 4], 
+            [3, 1, 7, 4],
             [2, 6, 5, 9],
             [8, 3, 3, 2]
         ])
         return supply, demand, costs
-    
+
     supply = [int(i) for i in input("Supplies: ").split()]
     demand = [int(i) for i in input("Demands: ").split()]
     costs = [[int(i) for i in input("Row of the costs: ").split()] for _ in range(len(supply))]
     return supply, demand, costs
+
 
 # что такое вектор начального базисного решения?
 # в этой программе вместо вектора выводится конечная матрица, по которой вычислялось решение
 # не проверено: когда методы не применимы?
 def main():
     supply, demand, costs = read_data()
-
+    supply1, demand1, costs1 = copy.deepcopy(supply), copy.deepcopy(demand), copy.deepcopy(costs)
     if sum(supply) == sum(demand) and len(supply) == 3 and len(demand) == 4:
         table_data = []
         for i, row in enumerate(costs):
@@ -40,14 +43,13 @@ def main():
         print(nw_x0)
         print("Initial basic feasible solution by North-west: ")
         print(nw_ans)
-
         vg_x0, vg_ans = vogels_approximation_method(supply, demand, costs)
         print("Vogel's approximation matrix")
         print(vg_x0)
         print("Initial basic feasible solution by Vogel's:")
         print(vg_ans)
-  
-        rus_x0, rus_ans = russells_approximation_method(supply, demand, costs)
+
+        rus_x0, rus_ans = russells_approximation_method(supply1, demand1, costs1)
         print("Russell's approximation matrix")
         print(rus_x0)
         print("Initial basic feasible solution by Russell's:")
@@ -57,6 +59,6 @@ def main():
     elif len(supply) != 3 or len(demand) != 4:
         print("The method is not applicable!")
 
-    
+
 if __name__ == "__main__":
     main()
